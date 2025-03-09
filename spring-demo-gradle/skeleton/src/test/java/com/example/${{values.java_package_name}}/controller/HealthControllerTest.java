2abf28f881e7d022c4cc4235b5cbdd82fb00f967
Alok Kulkarni <alokkulkarni@mac.lan>
2025-03-09 11:36:54 +0000

    initial springboot template

---
+++ spring-demo-gradle/skeleton/src/test/java/com/example/${{values.java_package_name}}/controller/HealthControllerTest.java
@@ -0,0 +1,34 @@
+package test.java.com.example.springdemogradle.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HealthController.class)
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHealthEndpoint() throws Exception {
+        mockMvc.perform(get("/api/health"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.status").value("UP"));
+    }
+
+    @Test
+    public void testInfoEndpoint() throws Exception {
+        mockMvc.perform(get("/api/info"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.app").value("Spring Demo Gradle"))
+               .andExpect(jsonPath("$.buildType").value("Gradle"))
+               .andExpect(jsonPath("$.features.sbom").value("enabled"))
+               .andExpect(jsonPath("$.features.actuator").value("enabled"));
+    }
+}
